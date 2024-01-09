597eab529d71ab214f40d5b2c41a25b1abe6717e
Theo Butler <theodusbutler@gmail.com>
2024-01-09 06:43:31 -0500

    chore: update subgraph client

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ use ethers::signers::{LocalWallet, Signer as _};
 use ethers::types::U256;
 use eventuals::{Eventual, EventualExt, Ptr};
 use serde::Deserialize;
+use thegraph::client::Client as SubgraphClient;
+use tokio::sync::Mutex;
 use toolshed::url::Url;
 
-use crate::{receipts::track_receipts, subgraph::spawn_poller};
+use crate::receipts::track_receipts;
 
 mod config;
 mod receipts;
-mod subgraph;
 
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
@@ -139,7 +140,7 @@ pub fn active_indexers(
     http_client: reqwest::Client,
     subgraph_endpoint: Url,
 ) -> Eventual<Ptr<HashSet<Address>>> {
-    let client = subgraph::Client::new(http_client, subgraph_endpoint, None);
+    let client = SubgraphClient::new(http_client, subgraph_endpoint);
     let query = r#"
         indexers(
             block: $block
@@ -167,7 +168,7 @@ pub fn escrow_accounts(
     subgraph_endpoint: Url,
     sender: Address,
 ) -> Eventual<Ptr<HashMap<Address, u128>>> {
-    let client = subgraph::Client::new(http_client, subgraph_endpoint, None);
+    let client = SubgraphClient::new(http_client, subgraph_endpoint);
     let query = format!(
         r#"
         escrowAccounts(
@@ -207,4 +208,28 @@ pub fn escrow_accounts(
             .collect();
         Ptr::new(entries)
     })
+}
+
+fn spawn_poller<T>(client: SubgraphClient, query: String) -> Eventual<Ptr<Vec<T>>>
+where
+    T: for<'de> Deserialize<'de> + Send + 'static,
+    Ptr<Vec<T>>: Send,
+{
+    let (writer, reader) = Eventual::new();
+    let state: &'static Mutex<_> = Box::leak(Box::new(Mutex::new((writer, client))));
+    eventuals::timer(Duration::from_secs(120))
+        .pipe_async(move |_| {
+            let query = query.clone();
+            async move {
+                let mut guard = state.lock().await;
+                match guard.1.paginated_query::<T>(query).await {
+                    Ok(response) => guard.0.write(Ptr::new(response)),
+                    Err(subgraph_poll_err) => {
+                        tracing::error!(%subgraph_poll_err, label = %std::any::type_name::<T>());
+                    }
+                };
+            }
+        })
+        .forever();
+    reader
 }
