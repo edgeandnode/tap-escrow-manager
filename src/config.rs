93b4aa725fdb27b5a35143bfa52e60a67a87ab7d
Theo Butler <theodusbutler@gmail.com>
2024-08-26 09:26:18 -0400

    docs: add comments to Config

--- src/config.rs
+++ src/config.rs
@@ -8,22 +8,37 @@ use thegraph_core::types::alloy_primitives::{Address, B256};
 #[serde_as]
 #[derive(Debug, Deserialize)]
 pub struct Config {
+    /// ID for the chain where the TAP escrow contract is deployed
     pub chain_id: u64,
+    /// Table of minimum debts by indexer. This can be used, for example, to account for receipts
+    /// missing from the kafka topic.
     pub debts: BTreeMap<Address, u64>,
+    /// TAP escrow contract address
     pub escrow_contract: Address,
+    /// TAP escrow subgraph
     #[serde_as(as = "DisplayFromStr")]
     pub escrow_subgraph: Url,
+    /// `graph_env` filter on the kafka topic for indexer receipts
     pub graph_env: String,
+    /// GRT contract for updating allowance
     pub grt_contract: Address,
+    /// GRT allowance to set on startup
     pub grt_allowance: u64,
+    /// Kafka configuration
     pub kafka: Kafka,
+    /// Graph network subgraph URL
     #[serde_as(as = "DisplayFromStr")]
     pub network_subgraph: Url,
+    /// API key for querying subgraphs
     pub query_auth: Hidden<String>,
+    /// RPC for executing transactions
     #[serde_as(as = "DisplayFromStr")]
     pub rpc_url: Hidden<Url>,
+    /// Secret key of the TAP sender wallet
     pub secret_key: Hidden<B256>,
+    /// Secret keys of the TAP signer wallets to authorize
     pub signers: Vec<Hidden<B256>>,
+    /// Period of the subgraph polling cycle
     pub update_interval_seconds: u32,
 }
 
