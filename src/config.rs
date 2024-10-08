44a586462a2163b0a13cb346d797a7bfca32cd37
Theo Butler <theodusbutler@gmail.com>
2024-10-08 10:44:50 -0400

    remove config debug

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
-use std::{collections::BTreeMap, fmt, ops::Deref, str::FromStr};
+use std::collections::BTreeMap;
 
 use alloy::primitives::{Address, B256};
 use reqwest::Url;
 use serde::Deserialize;
-use serde_with::{serde_as, DisplayFromStr};
+use serde_with::serde_as;
 
 #[serde_as]
-#[derive(Debug, Deserialize)]
+#[derive(Deserialize)]
 pub struct Config {
     /// Authorize signers on startup.
     pub authorize_signers: bool,
@@ -16,7 +16,7 @@ pub struct Config {
     /// TAP escrow contract address
     pub escrow_contract: Address,
     /// TAP escrow subgraph
-    #[serde_as(as = "DisplayFromStr")]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     pub escrow_subgraph: Url,
     /// GRT contract for updating allowance
     pub grt_contract: Address,
@@ -25,48 +25,23 @@ pub struct Config {
     /// Kafka configuration
     pub kafka: Kafka,
     /// Graph network subgraph URL
-    #[serde_as(as = "DisplayFromStr")]
+    #[serde_as(as = "serde_with::DisplayFromStr")]
     pub network_subgraph: Url,
     /// API key for querying subgraphs
-    pub query_auth: Hidden<String>,
+    pub query_auth: String,
     /// RPC for executing transactions
-    #[serde_as(as = "DisplayFromStr")]
-    pub rpc_url: Hidden<Url>,
+    #[serde_as(as = "serde_with::DisplayFromStr")]
+    pub rpc_url: Url,
     /// Secret key of the TAP sender wallet
-    pub secret_key: Hidden<B256>,
+    pub secret_key: B256,
     /// Secret keys of the TAP signer wallets, used to filter the indexer fees messages.
-    pub signers: Vec<Hidden<B256>>,
+    pub signers: Vec<B256>,
     /// Period of the subgraph polling cycle
     pub update_interval_seconds: u32,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Kafka {
-    pub config: Hidden<BTreeMap<String, String>>,
+    pub config: BTreeMap<String, String>,
     pub realtime_topic: String,
-    pub aggregated_topic: String,
-}
-
-#[derive(Deserialize)]
-#[serde(transparent)]
-pub struct Hidden<T>(T);
-
-impl<T: fmt::Debug> fmt::Debug for Hidden<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "HIDDEN")
-    }
-}
-
-impl<T: FromStr> FromStr for Hidden<T> {
-    type Err = T::Err;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(s.parse()?))
-    }
-}
-
-impl<T> Deref for Hidden<T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
 }
