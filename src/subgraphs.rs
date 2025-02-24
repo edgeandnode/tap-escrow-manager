9a8c84e50f6b21da6bac83285a645be10d1c532a
Theo Butler <theodusbutler@gmail.com>
2025-02-24 07:43:03 -0500

    feat: consume gateway_ravs topic (#71)

--- src/subgraphs.rs
+++ src/subgraphs.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use alloy::primitives::Address;
 use anyhow::anyhow;
@@ -84,32 +84,46 @@ pub async fn escrow_accounts(
     }
 }
 
-pub async fn active_indexers(
+pub struct Allocation {
+    pub id: Address,
+    pub indexer: Address,
+}
+
+pub async fn active_allocations(
     network_subgraph: &mut SubgraphClient,
-) -> anyhow::Result<HashSet<Address>> {
+) -> anyhow::Result<Vec<Allocation>> {
     let query = r#"
-        indexers(
+        allocations(
             block: $block
             orderBy: id
             orderDirection: asc
             first: $first
             where: {
                 id_gt: $last
-                allocationCount_gt: 0
+                status: Active
             }
         ) {
             id
+            indexer { id }
         }
     "#;
     #[derive(serde::Deserialize)]
-    struct Indexer {
+    struct Allocation_ {
+        id: Address,
+        indexer: Indexer_,
+    }
+    #[derive(serde::Deserialize)]
+    struct Indexer_ {
         id: Address,
     }
     Ok(network_subgraph
-        .paginated_query::<Indexer>(query, 500)
+        .paginated_query::<Allocation_>(query, 500)
         .await
         .map_err(|err| anyhow!(err))?
         .into_iter()
-        .map(|i| i.id)
+        .map(|a| Allocation {
+            id: a.id,
+            indexer: a.indexer.id,
+        })
         .collect())
 }
