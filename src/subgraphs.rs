8e8f6039b609ded0e8d5e7e68a13feca27b22655
Tomás Migone <tomas@edgeandnode.com>
2025-10-27 17:02:47 -0300

    fix: only consider horizon allocations
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- src/subgraphs.rs
+++ src/subgraphs.rs
@@ -101,6 +101,7 @@ pub async fn active_allocations(
             where: {
                 id_gt: $last
                 status: Active
+                isLegacy: false
             }
         ) {
             id
